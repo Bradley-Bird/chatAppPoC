f6de8cb0c3df31af4e347317daabc5169808fae3
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-05-26 18:07:51 -0700

    added to  test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,3 @@
-// import { render } from '@testing-library/react';
-// import { UserProvider } from './context/UserContext';
-// // import App from './App';
-// import { AuthProvider } from './context/AuthContext';
-// import Main from './views/Main';
 
 describe('example test', () => {
   it('first test', () => {
